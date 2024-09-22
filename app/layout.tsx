2c951eea5b5c7c3f8e6ab23e7aa6eae1d9fa1318
Akshat Dubey <akshatdubey0808@gmail.com>
2024-09-22 10:41:11 +0530

    added website preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { HomeDock } from "./_components/HomeDock";
 import { Toaster } from "sonner";
+import { HomeDock } from "./_components/HomeDock";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Akshat Dubey",
-  description: "Akshat Dubey's portfolio",
+  title: "ActuallyAkshat's Portfolio",
+  description: "A portfolio of Akshat Dubey.",
+  openGraph: {
+    images: [
+      {
+        url: "/portfolio.png",
+        width: 1200,
+        height: 630,
+        alt: "Nota Rapida",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "/portfolio.png",
+        width: 1200,
+        height: 630,
+        alt: "Nota Rapida",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
